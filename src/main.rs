a66eecf09f1e34e62d96aec4e8fc54444bacd1cd
Bwana Maina <johnmuhuhe@gmail.com>
2022-02-03 22:40:18 +0300

    api call and table

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,45 @@
+#[path = "functions/table.rs"]
+mod table;
+
+#[path = "structs/weather.rs"]
+mod weather;
+
+use dotenv_codegen::dotenv;
+
+use exitfailure::ExitFailure;
+use reqwest::Url;
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
-struct CLI {
+pub struct Cli {
     city: String,
     country_code: String,
 }
 
-fn main() {
-    let args = CLI::from_args();
+impl weather::Forecast {
+    async fn get(city: &str, country_code: &str) -> Result<Self, ExitFailure> {
+        let api_key = dotenv!("WEATHER_API_KEY");
 
-    println!("Getting weather for country : {} code : {}", args.city, args.country_code);
+        let url = format!(
+            "https://api.openweathermap.org/data/2.5/weather?q={},{}&appid={}",
+            city, country_code, api_key
+        );
+
+        let url = Url::parse(&*url)?;
+
+        let response = reqwest::get(url).await?.json::<weather::Forecast>().await?;
+
+        Ok(response)
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), ExitFailure> {
+    let args = Cli::from_args();
+
+    let response = weather::Forecast::get(&args.city, &args.country_code).await?;
+
+    table::draw_table(response);
+
+    Ok(())
 }
